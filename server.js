f19f6bf3c9db0731557066395375237f4876cbf5
elaine-luckey <emarieluckey@yahoo.com>
2023-09-19 17:54:41 -0400

    server.js update

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const session = require('express-session');
 const routes = require('./Develop/controllers');
 
 
-// const sequelize = require('./Develop/config/connection');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require('./Develop/config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = router();
 const PORT = process.env.PORT || 3001;
@@ -18,9 +18,9 @@ const sess = {
   },
   resave: false,
   saveUninitialized: true,
-  // store: new SequelizeStore({
-  //   db: sequelize
-  // })
+ store: new SequelizeStore({
+  db: sequelize
+ })
 };
 
 app.use(session(sess));
@@ -33,13 +33,13 @@ app.set('view engine', 'handlebars');
 app.use(router.json());
 app.use(router.urlencoded({ extended: true }));
 app.use(router.static('public'));
-//app.use(router.static(path.resolve(__dirname, 'public')));
+app.use(router.static(path.resolve(__dirname, 'public')));
 
 app.use(routes);
 
-/*sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
-*/
+
 
 module.exports = router;
